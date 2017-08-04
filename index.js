8d426b00251d4d8f2b869a796b3a34857b4cfac2
Anshuman <anshu.av97@gmail.com>
2017-08-04 17:06:15 +0530

    tweaks

--- index.js
+++ index.js
@@ -5,44 +5,42 @@ const electron = require('electron')
 const {app, BrowserWindow, globalShortcut} = electron
 
 // Adds debug features like hotkeys for triggering dev tools and reload
-require('electron-debug')();
+require('electron-debug')()
 
 // Prevent window being garbage collected
-let mainWindow, webContents;
+let mainWindow, webContents
 
-function onClosed() {
+function onClosed () {
 	// Dereference the window
 	// For multiple windows store them in an array
-	mainWindow = null;
+  mainWindow = null
 }
 
-function createMainWindow() {
-	const win = new BrowserWindow({
-		width: 1280,
-		height: 720,
-		frame: false
-	});
+function createMainWindow () {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    frame: false
+  })
 
-	win.loadURL(`file://${__dirname}/ongaku/index.html`);
-	win.on('closed', onClosed);
+  win.loadURL(`file://${__dirname}/ongaku/index.html`)
+  win.on('closed', onClosed)
 
-	return win;
+  return win
 }
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-});
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
 app.on('activate', () => {
-	if (!mainWindow) {
-		mainWindow = createMainWindow();
-	}
-});
+  if (!mainWindow) {
+    mainWindow = createMainWindow()
+  }
+})
 
 app.on('ready', () => {
-	mainWindow = createMainWindow();
-});
-
-
+  mainWindow = createMainWindow()
+})
