44f8b32bba2c1b62edf9a26f5ef91cb74d79d8e5
Anshuman <anshu.av97@gmail.com>
2017-08-04 16:13:35 +0530

    Made the app functional

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # ongaku-desktop
 
-Desktop port of ongaku written in electron
+> Electron based desktop port of [ongaku](https://github.com/Anshuman-Verma/ongaku).
+
+Play anime music with ease on your desktop.
+
+## Dev
+
+```
+$ npm install
+```
+
+## Run
+
+```
+$ npm start
+```
+
+If installed globally
+
+```
+$ ongaku
+```
+
+### Build
+
+```
+$ npm run build
+```
+
+Builds the app for macOS, Linux, and Windows, using [electron-packager](https://github.com/electron-userland/electron-packager).
+
+
+## License
+
+MIT © [Anshuman Verma](https://twitter.com/Anshumaniac12)
---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env electron
+
+const electron = require('electron')
+
+const {app, BrowserWindow, globalShortcut} = electron
+
+// Adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
+
+// Prevent window being garbage collected
+let mainWindow, webContents;
+
+function onClosed() {
+	// Dereference the window
+	// For multiple windows store them in an array
+	mainWindow = null;
+}
+
+function createMainWindow() {
+	const win = new BrowserWindow({
+		width: 1280,
+		height: 720,
+		frame: false
+	});
+
+	win.loadURL(`file://${__dirname}/ongaku/index.html`);
+	win.on('closed', onClosed);
+
+	return win;
+}
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (!mainWindow) {
+		mainWindow = createMainWindow();
+	}
+});
+
+app.on('ready', () => {
+	mainWindow = createMainWindow();
+});
+
+
