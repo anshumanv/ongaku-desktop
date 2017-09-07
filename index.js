d8cb64cbae99604450b92acdd18b2c2507b6ab38
Anshuman <anshu.av97@gmail.com>
2017-09-07 12:02:49 +0530

    Switched to tab indentation in index.js

--- index.js
+++ index.js
@@ -13,34 +13,34 @@ let mainWindow, webContents
 function onClosed () {
 	// Dereference the window
 	// For multiple windows store them in an array
-  mainWindow = null
+	mainWindow = null
 }
 
 function createMainWindow () {
-  const win = new BrowserWindow({
-    width: 1280,
-    height: 720,
-    frame: false
-  })
+	const win = new BrowserWindow({
+		width: 1280,
+		height: 720,
+		frame: false
+	})
 
-  win.loadURL(`file://${__dirname}/ongaku/index.html`)
-  win.on('closed', onClosed)
+	win.loadURL(`file://${__dirname}/ongaku/index.html`)
+	win.on('closed', onClosed)
 
-  return win
+	return win
 }
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
 
 app.on('activate', () => {
-  if (!mainWindow) {
-    mainWindow = createMainWindow()
-  }
+	if (!mainWindow) {
+		mainWindow = createMainWindow()
+	}
 })
 
 app.on('ready', () => {
-  mainWindow = createMainWindow()
+	mainWindow = createMainWindow()
 })
